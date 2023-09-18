a27020e2a497b5e7520e532492e609d991b97a15
mrkangs <gykang00@gmail.com>
2023-09-18 22:32:47 +0900

    Update some code

--- public/app.js
+++ public/app.js
@@ -2,9 +2,6 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("queryForm");
-    const imageContainer = document.getElementById("imageContainer");
-    const queryResult = document.getElementById("queryResult");
-    const generatePdfButton = document.getElementById("generatePdf");
 
     form.addEventListener("submit", async (event) => {
         event.preventDefault();
@@ -30,14 +27,6 @@ document.addEventListener("DOMContentLoaded", () => {
             queryResult.innerHTML += `<p>Birth Month: ${data.birthMonth}</p>`;
             queryResult.innerHTML += `<p>Birthday: ${data.birthday}</p>`;
 
-            // Display the image from Google Drive
-            const imageElement = document.createElement("img");
-            imageElement.src = data.imageUrl;
-            imageContainer.innerHTML = "<h2>Image from Google Drive</h2>";
-            imageContainer.appendChild(imageElement);
-
-            // Enable PDF generation button
-            generatePdfButton.disabled = false;
         } catch (error) {
             console.error(error);
             queryResult.innerHTML = "<p>An error occurred. Please try again.</p>";
