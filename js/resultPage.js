ded76a27b25f551f4a6f97b6d1c8bca1f31b74b4
BlueSky607 <52993086+BlueSky607@users.noreply.github.com>
2026-01-10 15:56:39 +0900

    Update resultPage.js

--- js/resultPage.js
+++ js/resultPage.js
@@ -108,8 +108,8 @@ document.addEventListener('DOMContentLoaded', function () {
         var soulCardTitleData = soulCardTitleTemplate({title: soulCards["c_id"]});
         var soulCardImageOneData = soulCardImageOneTemplate({imageOne: soulCards["c_image_1"]});
         var soulCardImageTwoData = soulCardImageTwoTemplate({imageTwo: soulCards["c_image_2"]});
-        var soulCardImageTwoData = soulCardImageTwoTemplate({imageTwo: soulCards["c_image_3"]});
-        var soulCardImageTwoData = soulCardImageTwoTemplate({imageTwo: soulCards["c_image_4"]});
+        var soulCardImageTwoData = soulCardImageTwoTemplate({imageThree: soulCards["c_image_3"]});
+        var soulCardImageTwoData = soulCardImageTwoTemplate({imageFour: soulCards["c_image_4"]});
         
 
         document.getElementById('soulCard').innerHTML += soulCardTitleData;
